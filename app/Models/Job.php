0533ee0e639c000df2a9bdb16a6ae3ae05266e35
NannZinMoegicjp <nannzinmoe.gicjp@gmail.com>
2023-05-01 17:31:40 +0630

    2023-05-01 evening

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+    public function address(){
+        return $this->belongsTo(Address::class);
+    }
+    public function employmentType(){
+        return $this->belongsTo(EmploymentType::class);
+    }
+    public function experienceLevel(){
+        return $this->belongsTo(ExperienceLevel::class);
+    }
+    public function jobCategory(){
+        return $this->belongsTo(JobCategory::class);
+    }
+    public function descriptions(){
+        return $this->hasMany(Description::class);
+    }
+    public function requirements(){ 
+        return $this->hasMany(Requirement::class);
+    }
+    public function benefits(){
+        return $this->hasMany(Benefit::class);
+    }
 }
