7b358f91bc12ec2054a6d24001caa7dd319c90c8
NannZinMoegicjp <nannzinmoe.gicjp@gmail.com>
2023-05-15 07:31:49 +0630

    latest v

--- resources/views/JobSeeker/master.blade.php
+++ resources/views/JobSeeker/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Job board</title>
+    <title>Job seeker dashboard</title>
     <link rel="stylesheet" href="{{URL::asset('css/admin.css')}}" />
     <link rel="stylesheet" href="{{URL::asset('css/order.css')}}" />
     <link rel="stylesheet" href="{{URL::asset('css/dashboard.css')}}" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
-    <title>Job board admin template</title>
+    <title>Admin dashboard</title>
     <link rel="stylesheet" href="{{URL::asset('css/admin.css')}}" />
     <link rel="stylesheet" href="{{URL::asset('css/order.css')}}" />
     <link rel="stylesheet" href="{{URL::asset('css/dashboard.css')}}" />
